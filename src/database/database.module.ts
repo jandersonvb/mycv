6a52e4d960fdd262c3d049cade581bbb338c1eb0
jandersonvb <jandersonvb.dev@gmail.com>
2024-02-14 13:15:19 -0300

    feat: create report entity

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
+      logging: true,
     }),
   ],
 })
